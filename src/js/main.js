f21ce6454d042c8dda0eef91ce3c75cfe95be967
NolbertoCastro <nolber0703@gmail.com>
2023-02-27 19:50:41 -0600

    changes

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,13 @@
 import '../scss/styles.scss'
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+
+// Activate Carousel
+$("#myCarousel").carousel();
+
+$(".left").click(function(){
+    $("#myCarousel").carousel("prev");
+});
+$(".right").click(function(){
+    $("#myCarousel").carousel("next");
+});
